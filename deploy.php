657ff06bdfba12eaf505241923652d96d3918e8e
zng42 <zng@alumni.cmu.edu>
2017-03-21 13:40:38 -0400

    fixed spacing on deploy.php

--- deploy.php
+++ deploy.php
@@ -1,31 +1,31 @@
 <?php
     /**
-         * GIT DEPLOYMENT SCRIPT
-	      *
-	           * Used for automatically deploying websites via GitHub
-		        *
-			     */
+     * GIT DEPLOYMENT SCRIPT
+     *
+     * Used for automatically deploying websites via GitHub
+     *
+     */
 
     // array of commands
-        $commands = array(
+    $commands = array(
 	        'echo $PWD',
-		        'whoami',
-			        'git pull',
-				        'git status',
-					        'git submodule sync',
-						        'git submodule update',
-							        'git submodule status',
-								    );
+	        'whoami',
+	        'git pull',
+	        'git status',
+	        'git submodule sync',
+	        'git submodule update',
+	        'git submodule status',
+       );
 
     // exec commands
-        $output = '';
-	    foreach($commands AS $command){
-	            $tmp = shell_exec($command);
+    $output = '';
+    foreach($commands AS $command){
+            $tmp = shell_exec($command);
 
-        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
-	        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
-		    }
-		    ?>
+            $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+            $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+    }
+?>
 
 <!DOCTYPE HTML>
 <html lang="en-US">
