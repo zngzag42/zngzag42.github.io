f549f7a22f44772d7ec9f269b0dfbe0f20c364b6
zng42 <zng@alumni.cmu.edu>
2017-09-29 13:00:40 -0400

    Updated Webpage

--- 848 E Scissors Congruence/notebookInfo.js
+++ 848 E Scissors Congruence/notebookInfo.js
@@ -1,3 +1,3 @@
 var staticUrl = 'http://new.notesplusapp.com/static';
 var notebook = {'name' : '848 E Scissors Congruence',
-                'pageCount' : 17 };
+                'pageCount' : 23 };
