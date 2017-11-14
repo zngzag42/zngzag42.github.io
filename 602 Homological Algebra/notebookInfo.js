e6e3a0a7e4395bd301a72e6f2da76f462d7dae91
zng42 <zng@alumni.cmu.edu>
2017-11-14 13:02:17 -0500

    Updated Webpage

--- 602 Homological Algebra/notebookInfo.js
+++ 602 Homological Algebra/notebookInfo.js
@@ -1,3 +1,3 @@
 var staticUrl = 'http://new.notesplusapp.com/static';
 var notebook = {'name' : '602 Homological Algebra',
-                'pageCount' : 46 };
+                'pageCount' : 57 };
--- 848 E Scissors Congruence/notebookInfo.js
+++ 848 E Scissors Congruence/notebookInfo.js
@@ -1,3 +1,3 @@
 var staticUrl = 'http://new.notesplusapp.com/static';
 var notebook = {'name' : '848 E Scissors Congruence',
-                'pageCount' : 39 };
+                'pageCount' : 44 };
