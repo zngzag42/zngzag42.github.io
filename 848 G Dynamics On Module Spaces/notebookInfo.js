9c28716f51eb1461745a0a26ae504e2de38d9d84
zng42 <zng@alumni.cmu.edu>
2019-05-15 15:39:32 -0400

    Updated Webpage

--- 848 G Dynamics On Module Spaces/notebookInfo.js
+++ 848 G Dynamics On Module Spaces/notebookInfo.js
@@ -1,3 +1,3 @@
 var staticUrl = 'http://new.notesplusapp.com/static';
 var notebook = {'name' : '848 G Dynamics On Module Spaces',
-                'pageCount' : 33 };
+                'pageCount' : 41 };
