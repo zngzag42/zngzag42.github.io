48382139b5e688ff081c2f9e915dc9794754bec1
zng42 <zng@alumni.cmu.edu>
2020-12-22 15:46:10 -0500

    Added Cluster Library umdwebsite

---
+++ ClusterLibrary/clusterNavigate.js
@@ -0,0 +1,10 @@
+function changeImage(id, newsrc)
+{
+    document.getElementById(id).src = newsrc;
+}
+
+function focusSubalgebra(srcEmbed, srcFocus)
+{
+    document.getElementById('embeded').src = srcEmbed;
+    document.getElementById('focused').src = srcFocus;
+}
