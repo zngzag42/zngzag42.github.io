fca985fa85b99cd7ab1a5710a2916d3578cc9c46
zng42 <zng@alumni.cmu.edu>
2020-12-22 16:29:20 -0500

    Updated Webpage

---
+++ 636 Introduction To Representation Theory/notebookInfo.js
@@ -0,0 +1,3 @@
+var staticUrl = 'http://new.notesplusapp.com/static';
+var notebook = {'name' : '636 Introduction To Representation Theory',
+                'pageCount' : 11 };
--- 742 Geometric Analysis/notebookInfo.js
+++ 742 Geometric Analysis/notebookInfo.js
@@ -1,3 +1,3 @@
 var staticUrl = 'http://new.notesplusapp.com/static';
 var notebook = {'name' : '742 Geometric Analysis',
-                'pageCount' : 12 };
+                'pageCount' : 16 };
