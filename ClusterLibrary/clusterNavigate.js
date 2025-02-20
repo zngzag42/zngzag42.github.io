04061ebfba4e9ae8c67508883223547ae163649e
zachary greenberg <greenberg@mis.mpg.de>
2025-02-20 10:37:28 +0100

    updated javascript to manage checkboxes in cluster explorer

--- ClusterLibrary/clusterNavigate.js
+++ ClusterLibrary/clusterNavigate.js
@@ -8,3 +8,29 @@ function focusSubalgebra(srcEmbed, srcFocus)
     document.getElementById('embeded').src = srcEmbed;
     document.getElementById('focused').src = srcFocus;
 }
+
+function updateFocus(a)
+{
+    const checkboxes = document.querySelectorAll('input[name="vertex"]:checked');
+    let values = [];
+    let disable = [];
+    checkboxes.forEach((checkbox) => {
+	values.push(checkbox.value.split('_')[0]);
+	disable.push(checkbox.value.split('_')[1].split(' '));
+    });
+    document.getElementById('embeded').src = 'graph_embeded_' + values.join('_')+'.png';
+    document.getElementById('focused').src = 'graph_focused_' + values.join('_')+'.png';
+    document.querySelectorAll('input[name="vertex"]').forEach((checkbox) => checkbox.disabled=false);
+    disable.flat().forEach((id) => {document.getElementById(id).disabled=true;});
+}
+
+function resetFocus()
+{
+    const checkboxes = document.querySelectorAll('input[name="vertex"]');
+    checkboxes.forEach((checkbox) =>
+	{
+	    checkbox.checked = false;
+	    checkbox.disabled= false;
+	});
+    updateFocus("");
+}
